58eb40848fb8e6acb24021ba2b5500384afb1165
Humaway <69836238+Humaway@users.noreply.github.com>
2026-04-12 14:46:38 +1000

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 // Get references to our HTML elements
